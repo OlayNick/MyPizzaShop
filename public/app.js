a8c72b767fb621e43d162db181aa58e3e06ab4fc
Nick Olay <vast87@mail.ru>
2014-09-30 23:41:20 +0400

    Add add_to_cart

--- public/app.js
+++ public/app.js
@@ -10,3 +10,14 @@ function something()
 
 	alert(x);
 }
+
+function add_to_cart(id)
+{
+	var key = 'product_' + id;
+
+	var x = window.localStorage.getItem(key);
+	x++;
+	window.localStorage.setItem(key, x);
+
+	//alert('You added pizza with id: ' + id);
+}
