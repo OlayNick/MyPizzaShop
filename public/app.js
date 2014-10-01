3856bea9de543b299f10d335a2007f9b9db79609
Nick Olay <vast87@mail.ru>
2014-10-01 21:55:00 +0400

    Add display baskets

--- public/app.js
+++ public/app.js
@@ -19,5 +19,28 @@ function add_to_cart(id)
 	x++;
 	window.localStorage.setItem(key, x);
 
+	get_number_products();
+
 	//alert('You added pizza with id: ' + id);
+}
+
+function get_number_products()
+{
+ 
+var total = 0;
+ 
+for (var i = 0; i < window.localStorage.length; i++) {
+    var key = window.localStorage.key(i);
+ 
+    if (key.search("prod")>=0)
+     
+    {
+        total = total*1 + window.localStorage[key]*1;
+ 
+    }
+ 
+         
+}
+document.getElementById("basket").innerHTML = "Your basket contains " + total + " items";
+ 
 }
