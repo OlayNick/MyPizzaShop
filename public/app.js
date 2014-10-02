9861465d0b80e3fa537ec97047eac03a1314eac3
Nick Olay <vast87@mail.ru>
2014-10-02 22:47:37 +0400

    Add cart element to navbar

--- public/app.js
+++ public/app.js
@@ -19,29 +19,46 @@ function add_to_cart(id)
 	x++;
 	window.localStorage.setItem(key, x);
 
-	get_number_products();
+	// get_number_products();
 
 	//alert('You added pizza with id: ' + id);
 }
 
-function get_number_products()
+function cart_get_number_of_items()
 {
- 
-var total = 0;
- 
-for (var i = 0; i < window.localStorage.length; i++) {
-    var key = window.localStorage.key(i);
- 
-    if (key.search("prod")>=0)
-     
-    {
-        total = total*1 + window.localStorage[key]*1;
- 
-    }
- 
-         
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // получаем ключ
+		var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby: hh[key] = x
+
+		if(key.indexOf('product_') == 0) 
+		{
+			cnt = cnt + value * 1
+		}
+	}
+	return cnt;
 }
 
-document.getElementById("basket").innerHTML = "Your basket contains " + total + " items";
+// function get_number_products()
+// {
  
-}
+// var total = 0;
+ 
+// for (var i = 0; i < window.localStorage.length; i++) {
+//     var key = window.localStorage.key(i);
+ 
+//     if (key.search("prod")>=0)
+     
+//     {
+//         total = total*1 + window.localStorage[key]*1;
+ 
+//     }
+ 
+         
+// }
+
+// document.getElementById("basket").innerHTML = "Your basket contains " + total + " items";
+ 
+// }
